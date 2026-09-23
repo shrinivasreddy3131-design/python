6a62014c5d6ac5c3342f8d7e2d7b65d7e79fe301
shrinivas jainapur <shrinivasreddy3131@gmail.com>
2026-09-23 22:38:44 +0530

    add another python practics

---
+++ day_3.py
@@ -0,0 +1,44 @@
+a=10
+b=10
+c=55
+print(a+b+c)
+
+a=b=c=2
+print(a+b+c)
+
+name="shrinivas"
+Name="rurtraj"
+print(name)
+
+name="shrinivas"#string
+age = 20#integer
+is_student = True#boolean
+weight = 55.5#float
+print(type(weight))
+
+age_Float = float(age)
+print(age_Float)
+s="100"
+print(int(s)+10)
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a+b-a*a)
+
+a=10
+b=20
+a=b
+b=a
+print(a)
+print(b)
