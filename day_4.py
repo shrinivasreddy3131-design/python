2033136293405031b3acd9f7abe3b928e9e640ba
shrinivas jainapur <shrinivasreddy3131@gmail.com>
2026-09-24 16:17:29 +0530

    day 4

---
+++ day_4.py
@@ -0,0 +1,11 @@
+print("shrinivas")
+
+age=input("age: ")
+print(age)
+
+boy_name=input("boy name: ")
+girl_name=input("girl name: ")
+print(boy_name)
+print(girl_name)
+print(boy_name,girl_name)
+print(boy_name+" loves "+girl_name)
