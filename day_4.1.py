f253695c8ad62c829d35d31f214c5b3bef49039e
shrinivas jainapur <shrinivasreddy3131@gmail.com>
2026-09-24 16:39:29 +0530

    day 4.1

---
+++ day_4.1.py
@@ -0,0 +1,7 @@
+boy_name=input("boy name: ")
+print(boy_name)
+print("hello,"+ boy_name)
+
+city_name=input("city name: ")
+age=input("age: ")
+print("you are"+age+" years old and live in "+city_name)
