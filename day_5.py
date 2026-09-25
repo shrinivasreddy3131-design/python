d6c6b3b5ba96007fc3e36245507eb79aebcec02d
shrinivas jainapur <shrinivasreddy3131@gmail.com>
2026-09-25 22:03:27 +0530

    day 5: python string practice

---
+++ day_5.py
@@ -0,0 +1,18 @@
+first_name="shrinivas"
+last_name="jainapur"
+full_name=first_name+ " "+last_name
+print(full_name)
+
+message = "warning! "
+print(message*4)
+
+print(message.upper())
+print(message.lower())
+print(message.strip())
+print(message.replace("warning","alart"))
+
+name = "shrinivas said' hello'"
+print(name)
+
+print(len(message))
+
